e779beec5e34c7a35dd063eee944cb93329add10
tunnckoCore <mameto_100@mail.bg>
2016-12-09 18:50:39 +0200

    chore(update): boilerplate

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * parse-function <https://github.com/tunnckoCore/parse-function>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://i.am.charlike.online)
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = function parseFunction () {
+
+}
---
+++ test.js
@@ -0,0 +1,18 @@
+/*!
+ * parse-function <https://github.com/tunnckoCore/parse-function>
+ *
+ * Copyright (c) Charlike Mike Reagent <@tunnckoCore> (http://i.am.charlike.online)
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
+
+const test = require('mukla')
+const parseFunction = require('./index')
+
+test('parse-function', function (done) {
+  parseFunction()
+  done()
+})
